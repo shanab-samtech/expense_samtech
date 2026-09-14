e293fc21ad34a056dc1c3fb0c962e93f5b7de826
Mostafa Shanab <m_shanab@samtech.me>
2026-09-14 14:21:08 +0300

    feat: Made Expense Entry Doctype

---
+++ expense_samtech/expense_samtech/doctype/expense_entry/expense_entry.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2026, Samtech Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Expense Entry", {
+	setup(frm) {
+		frm.set_query("cash_or_bank_account", () => ({
+			filters: {
+				account_type: ["in", ["Bank", "Cash"]],
+				is_group: 0,
+			},
+		}));
+	},
+});
---
+++ expense_samtech/expense_samtech/doctype/expense_entry/test_expense_entry.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Samtech Solutions and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestExpenseEntry(FrappeTestCase):
+	pass
